3cb269602952e8e86890a6cbba8d7313fc43a81e
saravenpi <saravenpi@yandex.com>
2020-08-28 13:35:38 +0200

    yay

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+const editJsonFile = require("edit-json-file");
+var stats = require(`${__dirname}/stats.json`)
 
+let file = editJsonFile(`${__dirname}/stats.json`);
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+  var additional = stats.hourcurrent + 1;
+  file.set(hourcurrent, additional);
 });
 
 http.listen(port, function(){
